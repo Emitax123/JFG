b039390e7c484b14bd7b87c0c6ea8df0a20420b0
Emitax <emanuelb918@gmail.com>
2025-01-22 12:12:36 -0300

    staticfiles

--- JFG/settings.py
+++ JFG/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'ProjectManager',
     'Accounting',
 ]
@@ -118,11 +119,17 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
-
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn JFG.wsgi
+web: python manage.py collectstatic && gunicorn JFG.wsgi
