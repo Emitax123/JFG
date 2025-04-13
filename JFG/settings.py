87606a960f2cf007aa2076be0efe98e4e21ef96a
Emitax123 <emanuelb_1998@outlook.com>
2025-04-13 18:32:36 -0300

    asd

--- JFG/settings.py
+++ JFG/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'JFG.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('Postgres.DATABASE_URL'))
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 # Password validation
