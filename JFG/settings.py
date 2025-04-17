c79be8cfa85df1015148d611c90aea492068d2f6
Emitax123 <emanuelb_1998@outlook.com>
2025-04-17 09:20:17 -0300

    Fix: Settings 7

--- JFG/settings.py
+++ JFG/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 #
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =os.getenv('DEBUG', default=False) == 'True'
+DEBUG =os.getenv('DEBUG', default=False, cast=bool)
 
 
 
