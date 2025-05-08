20b570c0b3b13637b8aa4de92b5d4acb7dbb5828
Emitax123 <emanuelb_1998@outlook.com>
2025-05-08 20:09:48 -0300

    FIx: Login Setings 9

--- Users/urls.py
+++ Users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import CustomLoginView
 
 urlpatterns = [
-    path('login/', CustomLoginView.as_view, name='login'),
+    path('login/', CustomLoginView.as_view(), name='login'),
 ]
 
