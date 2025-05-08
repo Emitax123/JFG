cb503a97fb814e50dc2944fe6af2df7532e03e7c
Emitax123 <emanuelb_1998@outlook.com>
2025-05-08 17:51:22 -0300

    FIx: Login Setings 5

--- Users/views.py
+++ Users/views.py
@@ -10,7 +10,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            #redurict to the previus pag
+            #redurict to the previus pagE
             return redirect(request.META.get('HTTP_REFERER', '/'))
            
         else:
