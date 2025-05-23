cf63e08929ae4b65d05def4ad1bbe4ea6100b98f
Emitax123 <emanuelb_1998@outlook.com>
2025-05-23 12:13:31 -0300

    Fix: Settings 12

--- JFG/settings.py
+++ JFG/settings.py
@@ -93,17 +93,17 @@ WSGI_APPLICATION = 'JFG.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-if not DEBUG:
-    DATABASES = {
+
+DATABASES = {
     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
+}
+
+'''DATABASES = {
     'default': {
        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-    }
+}'''
 
 
 # Access token from environment variable
