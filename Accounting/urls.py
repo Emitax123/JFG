fff311a4ffb361f00529d0872e6ed9fd5e955c09
Emitax123 <emanuelb_1998@outlook.com>
2025-08-21 22:26:16 -0300

    Fix: Accounting 1.1

--- Accounting/urls.py
+++ Accounting/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
   path('', views.accounting_mov_display, name='accounting_display'),
   path('<int:pk>/', views.accounting_mov_display, name='accounting_display'),
+  path('createentry/<int:pk>/', views.create_manual_acc_entry, name='accounting_create')
 ]
